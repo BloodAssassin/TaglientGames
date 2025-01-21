acbf4c77af1bd7ecfeec68a694c40ab28e579e10
BloodAssassin <rmilos65@gmail.com>
2025-01-21 22:27:07 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5000, console.log("Server is running on port 5000"));
+app.listen(port, console.log("Server is running on port 5000"));
