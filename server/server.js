39837d8fcac35e1a823c98d3155c0062f8b57c88
BloodAssassin <rmilos65@gmail.com>
2025-01-21 22:33:23 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -6,11 +6,61 @@ const app = express();
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 
+async function contact(user_name, user_email, user_message) {
+  // Create the message
+  const message = `
+  <h3>Contact Info</h3>
+  <b>Name:</b> ${user_name}<br>
+  <b>Email:</b> ${user_email}<br>
+  <h3>Message</h3>
+  <p>${user_message.replace(/\n/g, "<br>")}
+`;
+
+  // Create the transporter
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: email,
+      pass: password,
+    },
+  });
+
+  // Send the message
+  const info = await transporter.sendMail({
+    to: email,
+    subject: "Taglient Games - Contact Form",
+    html: message,
+  });
+
+  console.log("Message sent: " + info.messageId);
+}
+
+// API Calls
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.post("/api/contact", async (req, res) => {
+  const { user_name, user_email, user_message } = req.body;
+  try {
+    await contact(user_name, user_email, user_message);
+    res.status(200).send("Message sent successfully");
+  } catch (error) {
+    res.status(500).send("Failed to send message");
+    console.log(error.message);
+  }
+});
+
 // Serve the root directory page
 app.get("/", (req, res) => {
   res.send(
     "<h1>Welcome to Taglient Games</h1><p>This is the root directory page.</p>"
   );
+});
+
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, that route doesn't exist.");
 });
 
 // Listen for calls
