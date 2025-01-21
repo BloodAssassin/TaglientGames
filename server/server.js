88318411249b914c23a0c50e8eb4ccb686792ea3
BloodAssassin <rmilos65@gmail.com>
2025-01-21 22:36:45 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -58,6 +58,10 @@ app.get("/", (req, res) => {
   );
 });
 
+app.get("/easy", (req, res) => {
+  res.send("<h1>EASY</p>");
+});
+
 // Middleware to handle 404 errors
 app.use((req, res, next) => {
   res.status(404).send("Sorry, that route doesn't exist.");
