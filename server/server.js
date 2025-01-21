b54581cd0f5515ab37026d435d7caf20a3078aec
BloodAssassin <rmilos65@gmail.com>
2025-01-21 21:16:23 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.post("/api/contact", async (req, res) => {
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Serve the root directory page
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
