410a0e0fe60659069d29339ac96c5a490d2a20e1
BloodAssassin <rmilos65@gmail.com>
2025-01-21 21:23:12 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -53,9 +53,21 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 // Serve the root directory page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Handle all other GET requests by serving the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 // Middleware to handle 404 errors
@@ -66,5 +78,5 @@ app.use((req, res, next) => {
 // Listen for calls
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log("Server started on port 5000");
+  console.log(`Server started on port ${port}`);
 });
