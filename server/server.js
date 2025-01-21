8028b11ac20d490fc5848bf473ff9fd1f76e2b53
BloodAssassin <rmilos65@gmail.com>
2025-01-21 21:29:58 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -56,13 +56,14 @@ app.post("/api/contact", async (req, res) => {
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-// Handle all other GET requests by serving the React app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"), (err) => {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
+// Serve the root directory page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+  res.status(404).send("Sorry, that route doesn't exist.");
 });
 
 // Listen for calls
