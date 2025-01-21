6cbfd4d10a801039097a00fdf8f0d68bc2c6ef0c
BloodAssassin <rmilos65@gmail.com>
2025-01-21 21:38:54 +0100

    Update

--- server/server.js
+++ server/server.js
@@ -53,29 +53,6 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-// Serve static files from the React app
-app.use(
-  express.static(
-    path.join(__dirname, "https://taglient-games-client.vercel.app/")
-  )
-);
-
-// Handle all other GET requests by serving the React app
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "https://taglient-games-client.vercel.app/",
-      "index.html"
-    ),
-    (err) => {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
-
 // Middleware to handle 404 errors
 app.use((req, res, next) => {
   res.status(404).send("Sorry, that route doesn't exist.");
