8cf21b6bc654aa8f51584d7c7f1dc0a6aea5394a
BloodAssassin <rmilos65@gmail.com>
2025-01-26 19:04:58 +0100

    Test

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const nodeMailer = require("nodemailer");
-const cors = require("cors");
 const path = require("path");
 const app = express();
 
 const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
-
-// Enable CORS for all routes
-app.use(cors());
-app.options("*", cors()); // Enable preflight for all routes
 
 async function contact(user_name, user_email, user_message) {
   // Create the message
@@ -46,12 +41,19 @@ async function contact(user_name, user_email, user_message) {
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.post("/api/contact", async (req, res) => {
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // End preflight request here
+  }
+
+  // Handle the post request
   const { user_name, user_email, user_message } = req.body;
   try {
     await contact(user_name, user_email, user_message);
     res.status(200).send("Message sent successfully");
   } catch (error) {
     res.status(500).send("Failed to send message");
+    
     console.log(error.message);
   }
 });
